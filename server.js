ea1dc17abe091a59b19ebd269e8c6fad60c7ef51
Alyssa <alyssa.bawgus@gmail.com>
2024-04-24 12:35:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .send('Hello server is running')
+//     .end();
+// });
+
+const http = require('http');
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('test')
+})
 
 // Start the server
 const PORT = process.env.PORT || 8080;
