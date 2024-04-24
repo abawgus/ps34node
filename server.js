89f921cff4b0a0802bc28747437282e10a053cfd
Alyssa <alyssa.bawgus@gmail.com>
2024-04-24 12:56:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -12,18 +12,16 @@ const http = require('http');
 const fs = require('fs');
 
 const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });    
+  if (req.url === '/') {    
     fs.readFile('form.html', (err, data) => {
       if (err) {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(err);
+        console.log(err)
       } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
+        res.write(data);
       }
     });
-    
+    res.end('Ended')
   } else {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('404 Not Found');
