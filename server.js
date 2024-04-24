55895600e94707976e3417642260934a91749887
Alyssa <alyssa.bawgus@gmail.com>
2024-04-24 13:05:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,16 +13,25 @@ const fs = require('fs');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {    
-    // fs.readFile('form.html', (err, data) => {
-    //   if (err) {
-    //     // console.log(err)
-    //   } else {
-    //     res.writeHead(200, { 'Content-Type': 'text/html' });
-    //     res.write(data);
-    //   }
-    // });
+    var html = `
+    <!DOCTYPE html>
+      <html>
+      <head>
+      </head>
+      <body>
+        <h1>Problem set 3-4 </h1>
+        <h2>Node JS Web App #2</h2>
+
+        <form action="/process" method="post">
+          <label for="location">Place or Zip:</label>
+          <input type="text" id="location" name="location" required><br><br>    
+          </a><input type="submit" value="Submit">
+        </form>
+      </body>
+      </html>    
+    `;
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Ended')
+    res.end(html)
   } else {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('404 Not Found');
