fae2835d374d4ac1a70e94a6e524a13c8a218d83
Alyssa <alyssa.bawgus@gmail.com>
2024-04-24 12:38:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,16 @@
 // });
 
 const http = require('http');
+// const fs = require('fs');
+
 const app = http.createServer((req, res) => {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('test')
+  } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+  }
 })
 
 // Start the server
