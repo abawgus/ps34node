09b0f12dae383bd521d6cd0e9f048fdc8e4ed04d
Alyssa <alyssa.bawgus@gmail.com>
2024-04-24 10:40:40 -0400

    removed server start

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 8080;
+// server.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
