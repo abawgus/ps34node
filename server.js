6f0600590ab1b8a122dcaab8a421cedff02218be
Alyssa <alyssa.bawgus@gmail.com>
2024-04-24 13:21:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const http = require('http');
 const fs = require('fs');
 
-// const MongoClient = require('mongodb').MongoClient;
-// const url = "mongodb+srv://alyssabawgus:test1@cs120.fjcamx7.mongodb.net/?retryWrites=true&w=majority&appName=CS120";
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb+srv://alyssabawgus:test1@cs120.fjcamx7.mongodb.net/?retryWrites=true&w=majority&appName=CS120";
 
-// async function getLocInfo(query) {
-//   const client = new MongoClient(url);
-//   try {
-//       var dbo = client.db("PS34NodeJS");
-//       var places = dbo.collection('places');
-//       var qdata = await places.findOne(query, {projection: {_id: 0, place:1, zips:1}})
-//       return qdata
-//   } finally {
-//       await client.close()
-//   }      
-// }
+async function getLocInfo(query) {
+  const client = new MongoClient(url);
+  try {
+      var dbo = client.db("PS34NodeJS");
+      var places = dbo.collection('places');
+      var qdata = await places.findOne(query, {projection: {_id: 0, place:1, zips:1}})
+      return qdata
+  } finally {
+      await client.close()
+  }      
+}
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {    
@@ -50,23 +50,22 @@ const app = http.createServer((req, res) => {
         const formData = new URLSearchParams(body);
         const location = formData.get('location');
         
-      //   console.log('Input:', location);
-        
-      //   if (/^\d+$/.test(location[0])) {
-      //     console.log('Zip')
-      //     query = {zips:location};
-      //   } else {
-      //     console.log('Place')
-      //     query = {place:location};
-      //   }
-      //   getLocInfo(query).then(qdata =>{
-      //     console.log(qdata)
-      //     res.write('<b>Place:</b> '+qdata.place)
-      //     res.write('<br>')
-      //     res.write('<b>Zips:</b> '+qdata.zips.join(', '))
+        if (/^\d+$/.test(location[0])) {
+          console.log('Zip')
+          query = {zips:location};
+        } else {
+          console.log('Place')
+          query = {place:location};
+        }
+
+        // getLocInfo(query).then(qdata =>{
+        //   console.log(qdata)
+        //   res.write('<b>Place:</b> '+qdata.place)
+        //   res.write('<br>')
+        //   res.write('<b>Zips:</b> '+qdata.zips.join(', '))
           
-      //     res.end('');
-      //   })
+        //   res.end('');
+        // })
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(location);
         res.end('End Process Screen');
